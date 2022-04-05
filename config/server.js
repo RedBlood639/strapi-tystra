42387e6d2ca396904d1b5a412797979ada7765f7
Redblood639 <albinorabbithunter@gmail.com>
2022-04-06 04:42:55 +0800

    feat: push

--- config/server.js
+++ config/server.js
@@ -1,7 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  url: env("MY_HEROKU_URL"),
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", [
+      "aSvsp74CKI1oRzqO+026RA==",
+      "3cxux7TbxxKbqUu/GZ+1Xg==",
+      "7pPkxBFqhEvazYE066vH7w==",
+      "aA6Or0vs4sHHjFqUupaeaw==",
+    ]),
   },
 });
